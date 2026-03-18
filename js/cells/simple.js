5fa6c2e16ca35c5ad592f1718e3efee4ac9bb45d
DAOTUU <122334161+DAOTUU@users.noreply.github.com>
2026-03-19 02:29:48 +0800

    init project cells

---
+++ js/cells/simple.js
@@ -0,0 +1,10 @@
+CellRegistry.register({
+  id: 'simple',
+  name: '简单立方',
+  data: {
+    corners: [[1,1,1],[-1,1,1],[1,-1,1],[-1,-1,1],[1,1,-1],[-1,1,-1],[1,-1,-1],[-1,-1,-1]],
+    faces: [],
+    inner: [],
+    bonds: []
+  }
+})
